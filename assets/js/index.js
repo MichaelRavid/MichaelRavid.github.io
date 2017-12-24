9feaff93418d14ad5cc192c24a0d76f494a0ab96
MichaelRavid <Michael.Ravid@hotmail.com>
2017-12-24 13:20:16 -0500

    Final Deployment
    Re routed logo, added second modal, java typing effect and fade in/out effect for transition

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,17 @@
-var portPic= document.querySelector("ul")
+//Animation for header
 
-console.log(portPic);
+var i =1;
+
+function func(){
+	var message="Responsive Web Design"
+	if (i <= message.length){
+		var txt = message.substring(0,i);
+		document.getElementById("home-text").innerHTML = txt;
+		setTimeout("func()",150)
+		i++
+	}
+	else{
+	}
+}
+func();
 
